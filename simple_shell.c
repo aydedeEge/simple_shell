cd7f37a501f5b1e96c91be8e20b2c5d099ffd234
EgeAydede <ege.aydede@mail.mcgill.ca>
2016-10-08 00:17:20 -0400

    Display history w/ history command

--- simple_shell.c
+++ simple_shell.c
@@ -30,23 +30,67 @@ int getcmd(char *prompt, char *args[], int *background) {
 		}
 		return i; 
 	}
+
+void storehistory(char *args[]){
+
+}
+struct command{
+		char *args[20];
+		int count;
+		int length;
+};
+
+
 int main(void) {
 	char *args[20]; int bg;
+	char *history_args[20];
+	int t_count = -1;
+	struct command all_commands[10];
+	
 	while(1) {
 	    bg = 0;
 		int cnt = getcmd("\n>> ", args, &bg);
+		//Incriment # of commands
+		t_count++;
 		/* the steps can be..:
 		(1) fork a child process using fork()
 		(2) the child process will invoke execvp()
 		(3) if background is not specified, the parent will wait,
 			otherwise parent starts the next command... */
+
+		//Make sure last args entry is null
 		args[cnt] = NULL;
+
+		struct command commandi;
+
+		commandi.count = t_count;
+		commandi.length = cnt;
+		for(int i=0; i<cnt; i++){
+			commandi.args[i] = args[i];
+		}
+
+		all_commands[t_count%10] = commandi;
 
 		pid_t process = fork();
 		if(process==0) {
 			//child
-			execvp(args[0], args);
-			exit(0);
+			if(!strcmp(args[0], "history")){
+				for(int i=t_count; i>t_count-10 && i>0; i--){
+					printf("%d : ", all_commands[i%10].count);
+
+					for(int j=0; j<all_commands[i%10].length; j++){
+						printf("%s", all_commands[i%10].args[j]);
+					}
+
+					printf("\n");
+				}
+			}else if(args[0][0]=='!') {
+				printf("Got here");
+			}else {
+				execvp(args[0], args);
+				exit(0);
+			}
+			
 		}else {
 			//parent
 			if(!bg){
