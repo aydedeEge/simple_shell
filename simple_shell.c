13f2bb23c73ba247c619c100e20333342012dfb8
EgeAydede <ege.aydede@mail.mcgill.ca>
2016-10-09 20:39:19 -0400

    Redirect working with write only

--- simple_shell.c
+++ simple_shell.c
@@ -2,6 +2,7 @@
 #include <unistd.h> 
 #include <string.h> 
 #include <stdlib.h>
+#include <fcntl.h>
 //
 // This code is given for illustration purposes. You need not include or follow this
 // strictly. Feel free to writer better or bug free code. This example code block does not
@@ -32,9 +33,16 @@ int getcmd(char *prompt, char *args[], int *background) {
 	}
 
 struct command{
-		char *args[20];
-		int count;
-		int length;
+	char *args[20];
+	int count;
+	int length;
+};
+
+struct BPROCESS{
+	char *args[20];
+	int pid;
+	int length;
+	int uid;
 };
 
 struct command getHistory(char *id, struct command all_c[10]){
@@ -55,11 +63,53 @@ struct command getHistory(char *id, struct command all_c[10]){
 	return all_c[selector];
 }
 
+void changeDir(char *arg){
+	int success = chdir(arg);
+	if(!success){
+		printf("cwd -> %s\n", arg);
+	}else{
+		printf("Dir does not exist or is not dir");
+	}
+}
+
+void currentDir(){
+	size_t size = 100;
+	char *buffer = (char *) malloc(size);
+	if(getcwd(buffer, size)==buffer){
+		 printf("%s\n", buffer);
+	}
+	free(buffer);
+}
+
+void getJobs(struct BPROCESS all_bp[2]){
+	// int status;
+	// waitpid(all_bp[0].pid, &status, WNOHANG);
+	for(int i=0; i<2; i++){
+		
+		int status;
+		waitpid(all_bp[i].pid, &status, WNOHANG);
+		if(status!=0 && all_bp[i].pid!=0){
+			printf("%d : %d : ", all_bp[i].uid, all_bp[i].pid);
+			for(int k=0; k<all_bp[i].length; k++){
+				printf("%s ", all_bp[i].args[k]);
+			}
+			printf("\n");
+		}
+	}
+}
+
 int main(void) {
 	char *args[20]; int bg;
 	struct command history_command;
-	int t_count = -1;
 	struct command all_commands[10];
+	struct BPROCESS all_bprocesses[2];
+	struct command commandi;
+	struct BPROCESS bprocessi;
+	int t_count = -1;
+	int bprocess_count = 1;
+	int redirect = 0;
+	int stdout_cpy;
+	int output_file;
 	
 	while(1) {
 	    bg = 0;
@@ -72,7 +122,7 @@ int main(void) {
 		(3) if background is not specified, the parent will wait,
 			otherwise parent starts the next command... */
 
-		
+		/*History command access*/
 		if(args[0][0]=='!') {
 			history_command = getHistory(args[0], all_commands);
 			for(int k=0; k<history_command.length; k++){
@@ -81,21 +131,42 @@ int main(void) {
 			cnt = history_command.length;
 		}
 
+		if(!cnt){
+			continue;
+		}
+
 		//Make sure last args entry is null
 		args[cnt] = NULL;
-		struct command commandi;
 
+		/*Storing last 10 commands for history*/
 		commandi.count = t_count;
 		commandi.length = cnt;
 		for(int i=0; i<cnt; i++){
 			commandi.args[i] = args[i];
 		}
 
+		/*Circular array with modulus operator*/
 		all_commands[t_count%10] = commandi;
+
+		/*Built in commands*/
+		if(!strcmp(args[0], "cd")){
+			changeDir(args[1]);
+			continue;
+		}else if(!strcmp(args[0], "pwd")){
+			currentDir();
+			continue;
+		}else if(!strcmp(args[0], "exit")){
+			exit(0);
+		}else if(!strcmp(args[0], "jobs")){
+			getJobs(all_bprocesses);
+			continue;
+		}
 
 		pid_t process = fork();
 		if(process==0) {
-			//child
+			//CHILD
+
+			/*Print history from all commands list*/
 			if(!strcmp(args[0], "history")){
 				for(int i=t_count; i>t_count-10 && i>-1; i--){
 					printf("%d : ", all_commands[i%10].count);
@@ -106,16 +177,40 @@ int main(void) {
 
 					printf("\n");
 				}
-			}else {
+			}else{
+				/*Check for redirect*/
+				for(int t=0; t<cnt; t++){
+					if(!strcmp(args[t], ">")){
+						cnt = t;
+						close(1);
+						output_file = open(args[t+1], O_WRONLY);
+						args[cnt] = NULL;
+					}else if(!strcmp(args[t], "|")){
+						cnt = 1;
+
+					}
+				}
 				execvp(args[0], args);
-				exit(0);
 			}
+
+			exit(0);
 			
 		}else {
-			//parent
+			//PARENT
 			if(!bg){
 				int status;
 				waitpid(process, &status, 0);
+			}else{
+				// /*New background process to be added to bprocess_list*/
+				all_bprocesses[bprocess_count-1].uid = bprocess_count;
+				all_bprocesses[bprocess_count-1].pid = process;
+				all_bprocesses[bprocess_count-1].length = cnt;
+				for(int l=0; l<cnt; l++){
+					all_bprocesses[bprocess_count-1].args[l] = args[l];
+				}
+				all_bprocesses[bprocess_count-1].args[cnt] = NULL;
+				// printf("%d\n", all_bprocesses[bprocess_count-1].pid);
+				bprocess_count++;
 			}
 		}
 	}
